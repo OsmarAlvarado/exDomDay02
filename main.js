391d8af1c4ea2c3cf599baf8c0f7fcc24780bebe
OsmarAlvarado <osmar.alvarado.dev@gmail.com>
2022-11-16 16:57:21 +0100

    in home

---
+++ main.js
@@ -0,0 +1,14 @@
+const all = document.querySelector('body')
+const input1 = document.querySelector('#text1')
+const box = document.querySelector('#box-types')
+const typeBox = document.querySelectorAll('.typebox')
+const selectSmall = document.querySelector('#small')
+const selectMed = document.querySelector('#med')
+const selectBig = document.querySelector('#big')
+const area = document.querySelector('textarea')
+const sendBt = document.querySelector('button')
+const information = document.querySelector('.inf')
+
+//console.log(typeBox);
+
+//console.log(all);
